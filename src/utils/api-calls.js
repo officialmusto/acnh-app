ac9edeee051160a251090eabe4c3cbf8afb0b9c6
Christian Musto <christianmustoj@gmail.com>
2024-10-11 15:58:27 -0400

    add async fish function, add async fish function, add async bug function, add err handling.

--- src/utils/api-calls.js
+++ src/utils/api-calls.js
@@ -20,3 +20,60 @@ export async function getVillagers() {
     throw error
   }
 }
+
+export async function getFossils() {
+  try {
+    const response = await fetch(`${BASE_URL}/nh/fossils/all`, {
+      headers: {
+        'X-API-KEY': API_KEY,
+      },
+    })
+
+    // Wait for the response and parse it as JSON
+    const data = await response.json()
+
+    // Return the data
+    return data
+  } catch (error) {
+    console.error("Failed to load fossils:", error)
+    throw error
+  }
+}
+
+export async function getFishes() {
+  try {
+    const response = await fetch(`${BASE_URL}/nh/fish`, {
+      headers: {
+        'X-API-KEY': API_KEY,
+      },
+    })
+
+    // Wait for the response and parse it as JSON
+    const data = await response.json()
+
+    // Return the data
+    return data
+  } catch (error) {
+    console.error("Failed to load fish:", error)
+    throw error
+  }
+}
+
+export async function getBugs() {
+  try {
+    const response = await fetch(`${BASE_URL}/nh/bugs`, {
+      headers: {
+        'X-API-KEY': API_KEY,
+      },
+    })
+
+    // Wait for the response and parse it as JSON
+    const data = await response.json()
+
+    // Return the data
+    return data
+  } catch (error) {
+    console.error("Failed to load bugs:", error)
+    throw error
+  }
+}
