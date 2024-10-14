64deba7740bcc2b68bfbdf45448614dfde900d3e
Christian Musto <christianmustoj@gmail.com>
2024-10-14 17:18:43 -0400

    update villager styling to render as 3 separate elements. Update villager index to include personalities.

--- src/components/Villager/Villager.jsx
+++ src/components/Villager/Villager.jsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 import styles from './Villager.styles'
 import React from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6'
 
 const Villager = ({ villager }) => {
   if (!villager.nh_details || !villager.nh_details.icon_url) {
@@ -18,17 +19,25 @@ const Villager = ({ villager }) => {
 
         <Text style={[styles.name, { color: `#${villager.title_color}`}]}>{villager.name}</Text>
         <View style={styles.infoSection}>
+
           <View style={styles.iconTextRow}>
-            <Icon name="birthday-cake" size={13.575} color={`#${villager.title_color}`}/>
+            <Icon name="birthday-cake" size={10.575} color={`#${villager.title_color}`}/>
             <Text style={styles.infoText}>
               {villager.birthday_month} {villager.birthday_day}
             </Text>
           </View>
 
           <View style={styles.iconTextRow}>
-            <Icon name="comment" size={14} color={`#${villager.title_color}`} />
+            <Icon name="comment" size={11} color={`#${villager.title_color}`} />
             <Text style={styles.infoText}>"{villager.phrase}"</Text>
           </View>
+
+          <View style={styles.iconTextRow}>
+          <FontAwesome6 name="person-rays" size={12.7} color={`#${villager.title_color}`} />
+            <Text style={styles.infoText}>"{villager.personality}"</Text>
+          </View>
+
+
         </View>
       </View>
     </View>
