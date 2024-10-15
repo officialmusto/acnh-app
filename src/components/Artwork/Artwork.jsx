c2922d21da034d08f7a65e66508735e9c9754f1d
Christian Musto <christianmustoj@gmail.com>
2024-10-14 23:59:21 -0400

    update acnh artwork to contain same aesthetic as all other index lists.

--- src/components/Artwork/Artwork.jsx
+++ src/components/Artwork/Artwork.jsx
@@ -7,7 +7,7 @@ const Artwork = ({ artwork, poster }) => {
   const selectedPoster = poster ? poster.variations[0] : null
 
   return (
-    <SafeAreaView style={styles.body}>
+    <SafeAreaView style={styles.card}>
       <Text style={styles.text}>{`${artwork.name} & ${poster ? poster.name : "poster not found"}`}</Text>
       
       <View style={styles.imageContainer}>
--- src/components/Artwork/Artwork.styles.jsx
+++ src/components/Artwork/Artwork.styles.jsx
@@ -1,25 +1,36 @@
 import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
-  body: {
-    borderWidth: 2,
-    padding: 10,
+  card: {
     margin: 13,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingLeft: 12,
+    backgroundColor: '#faf3ec',
+    borderRadius: 29,
+    marginVertical: 10,
+    shadowColor: '#151615',
+    shadowOffset: { width: 0, height: 14 },
+    shadowOpacity: .149,
+    shadowRadius: 2,
   },
   imageContainer: {
+    flex: 1,
     alignItems: 'center',
     flexDirection: 'row'
   },
   photo: {
+    flex: 1,
     width: 100,
     height: 120,
   },
   poster: {
+    flex: 1,
     width: 100,
     height: 100,
   },
+  text: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  }
 
 })
 
