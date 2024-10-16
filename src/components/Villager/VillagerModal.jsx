e4866899b9b31626a23114c76e14c94c58157dca
Christian Musto <christianmustoj@gmail.com>
2024-10-15 23:42:49 -0400

    update modal to include 3 traits and style horizontally.

--- src/components/Villager/VillagerModal.jsx
+++ src/components/Villager/VillagerModal.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import { SafeAreaView, View, Text, Image, Modal, Button } from 'react-native'
 import styles from './VillagerModal.styles'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6'
 
 const VillagerModal = ({ villager, visible, onClose }) => (
   <Modal visible={visible} animationType="slide" transparent={true} onRequestClose={onClose}>
     <SafeAreaView style={styles.modalOverlay}>
       <View style={styles.modalContent}>
-        <Text style={styles.modalTitle}>{villager.name}'s Details</Text>
+        <Text style={styles.modalTitle}>{villager.name}'s Details.</Text>
         <Image source={{ uri: villager.nh_details.image_url }} style={styles.modalImage} resizeMode="contain" />
-        <Text style={styles.modalText}>Birthday: {villager.birthday_month} {villager.birthday_day}</Text>
-        <Text style={styles.modalText}>Phrase: "{villager.phrase}"</Text>
-        <Text style={styles.modalText}>Personality: {villager.personality}</Text>
+
+        <View style={styles.iconSection}>
+
+          <View style={styles.iconTextRow}>
+            <Icon name='birthday-cake' size={17.575} color={`#${villager.title_color}`} />
+            <Text style={styles.infoText}>{villager.birthday_month} {villager.birthday_day}</Text>
+          </View>
+
+          <View style={styles.iconTextRow}>
+            <Icon name="comment" size={17} color={`#${villager.title_color}`} />
+            <Text style={styles.infoText}>"{villager.phrase}"</Text>
+          </View>
+
+          <View style={styles.iconTextRow}>
+            <FontAwesome6 name="person-rays" size={19.7} color={`#${villager.title_color}`} />
+            <Text style={styles.infoText}>"{villager.personality}"</Text>
+          </View>
+
+        </View>
+
         <Button title="Close" onPress={onClose} />
       </View>
     </SafeAreaView>
--- src/components/Villager/VillagerModal.styles.jsx
+++ src/components/Villager/VillagerModal.styles.jsx
@@ -22,13 +22,34 @@ const styles = StyleSheet.create({
   },
   modalImage: {
     width: 100,
-    height: 100,
+    height: 170,
+  },
+  iconSection: {
+    margin: 13,
+    flexDirection: 'row',
+  },
+  iconTextRow: {
+    margin: 3,
+    backgroundColor: '#3f3c39',
+    borderRadius: 32,
+    paddingTop: 9,
+    paddingBottom: 9,
+    paddingLeft: 14,
+    paddingRight: 14,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  infoText: {
+    paddingLeft: 10,
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: '#faf3ec',
   },
   modalText: {
     fontSize: 16,
     marginBottom: 10,
     textAlign: 'center',
-  },
+  }
 })
 
 export default styles
